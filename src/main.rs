eddc009c76f88d7f3ca21cb266043188d026f77a
cyla00 <ikhayam000@protonmail.com>
2023-10-20 01:20:31 +0200

    db connection

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 pub mod routes;
 use routes::*;
 
+mod mongo;
+
 #[allow(unused_imports)]
 use axum::{
     routing::{get, post},
@@ -13,13 +15,32 @@ use axum::{
 use serde::{Deserialize, Serialize};
 
 #[allow(unused_imports)]
-use mongodb::{Client, options::ClientOptions};
+use mongodb::{Client, options::ClientOptions, error::Result, bson::doc};
 
 use std::net::SocketAddr;
+
+#[derive(Serialize, Clone)]
+struct User{
+    username: String,
+    email: String,
+}
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
+
+    let db = mongo::database_connection();
+
+    let user:User = User{
+        username: "testUser".to_owned(),
+        email: "email@mail.com".to_owned(),
+    };
+
+    let _ = db.await.collection("testCollection").insert_one(user.clone(), None).await;
+    println!("email: {} added to database", user.email.clone());
+
+
+    // API routes
     let app = Router::new()
         .route("/login", post(route_login))
         .route("/registration", post(route_registration));
